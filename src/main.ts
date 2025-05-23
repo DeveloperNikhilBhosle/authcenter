772a0a6982b832b108c8c0b16023a0530c959fd4
Nikhil <nikhil.bhosle@atriina.com>
2025-05-23 14:42:59 +0530

    NIKHIL :: Added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
 
 
 async function bootstrap() {
@@ -16,6 +18,31 @@ async function bootstrap() {
   });
 
   console.log(process.env.AUTH_CENTER_DATABASE_URL, 'process.env.AUTH_CENTER_DATABASE_URL Main.TS');
+
+  //#region SWAGGER 
+
+  const config = new DocumentBuilder()
+    .setTitle('AuthCenter')
+    // .setDescription('API description')
+    // .setVersion('1.0')
+    // .addTag('example') // Optional: Group your endpoints by tags
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI served at /api
+
+  // SwaggerModule.setup('/swagger-json', app, document); // serve JSON separately
+
+  // Serve static assets (e.g., logo) if needed
+  // app.use('/assets', express.static(join(__dirname, '..', 'assets')));
+
+  // Serve custom HTML
+  // app.use('/api', (req, res) => {
+  //   const html = readFileSync(join(__dirname, 'public', 'swagger-custom.html'), 'utf8');
+  //   res.send(html);
+  // });
+
+  //#endregion
 
   await app.listen(process.env.PORT ?? 3000);
 }
